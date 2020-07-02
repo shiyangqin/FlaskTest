bf0cac58a141517575ca606d829671702addfcb9
shiyangqin <shiyangqin@yeah.net>
2020-07-02 15:12:13 +0800

    更新

--- logging_config.py
+++ logging_config.py
@@ -2,7 +2,7 @@
 
 import logging
 
-import logging.handlers
+from logging.handlers import RotatingFileHandler
 
 import os
 
@@ -16,45 +16,35 @@ LEVELS = {
 }
 
 
-def config_logging(file_name="log.txt", log_level="NOSET"):
+def logging_config(file_name="log.log", log_level="NOSET"):
+    """日志配置
+    :param file_name:日志文件名，默认在同级目录logs下
+    :param log_level:日志等级
+    :return:None
     """
-    @summary: config logging to write logs to local file
-    @param file_name: name of log file
-    @param log_level: log level
-    """
+    # 创建文件夹
     logs_dir = os.path.join(os.path.dirname(__file__), "logs")
     if os.path.exists(logs_dir) and os.path.isdir(logs_dir):
         pass
     else:
         os.makedirs(logs_dir)
+    file_name = os.path.join(logs_dir, file_name)
 
     logging.getLogger("").handlers = []
 
-    file_name = os.path.join(logs_dir, file_name)
-    rotating_file_handler = \
-        logging.handlers.RotatingFileHandler(filename=file_name)
-    formatter = logging.Formatter("%(asctime)s %(name)-12s %(process)d %(levelname)-8s %(lineno)-6s %(message)s")
-    '''
-    format: 指定输出的格式和内容，format可以输出很多有用信息，如上例所示:
-     %(levelno)s: 打印日志级别的数值
-     %(levelname)s: 打印日志级别名称
-     %(pathname)s: 打印当前执行程序的路径，其实就是sys.argv[0]
-     %(filename)s: 打印当前执行程序名
-     %(funcName)s: 打印日志的当前函数
-     %(lineno)d: 打印日志的当前行号
-     %(asctime)s: 打印日志的时间
-     %(thread)d: 打印线程ID
-     %(threadName)s: 打印线程名称
-     %(process)d: 打印进程ID
-     %(message)s: 打印日志信息
-    '''
+    # 设置RotatingFileHandler
+    rotating_file_handler = RotatingFileHandler(filename=file_name, maxBytes=1024 * 1024 * 50, backupCount=3)
+    formatter = logging.Formatter("%(asctime)s %(name)s %(process)d %(levelname)s %(lineno)s %(message)s")
     rotating_file_handler.setFormatter(formatter)
     logging.getLogger("").addHandler(rotating_file_handler)
 
+    # 设置StreamHandler
     console = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(name)-12s %(process)d %(levelname)-8s %(lineno)-6s %(message)s")
+    formatter = logging.Formatter("%(asctime)s %(name)s %(process)d %(levelname)s %(lineno)s %(message)s")
     console.setFormatter(formatter)
     logging.getLogger("").addHandler(console)
+
+    # 设置日志打印级别
     logger = logging.getLogger("")
     level = LEVELS[log_level.upper()]
     logger.setLevel(level)
